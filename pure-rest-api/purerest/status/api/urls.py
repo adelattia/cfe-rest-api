035b6f2039d751158329a2c201b025fd4e6c2fa3
Adel <attia3adel@gmail.com>
2018-02-19 23:48:59 +0100

    api endpoints

---
+++ pure-rest-api/purerest/status/api/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^$', StatusListSearchAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
+    url(r'(?P<id>.*)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
+]
+
+
+# 1
+
+# /api/status/ -> list
+# /api/status/create/ -> create
+# /api/status/id/ -> detail
+# /api/status/id/update -> update
+# /api/status/id/delete -> delete
+
+
+# 2
+# /api/status/ -> list
+# /api/status/id/ -> crud
+
+
+#3
+# /api/status/ -> crud and list/search
