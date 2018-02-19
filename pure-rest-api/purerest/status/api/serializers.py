b84fe2147cc5728873985e2c5109b0de88fd4604
Adel <attia3adel@gmail.com>
2018-02-19 22:26:24 +0100

    validation and fields

--- pure-rest-api/purerest/status/api/serializers.py
+++ pure-rest-api/purerest/status/api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from status.models import Status
 
 
+'''
+class CustomSerializer(serializers.Serializer):
+    content = serializers.CharField()
+    email = serializers.EmailField()
+
+can be used without saving
+'''
+
 class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
@@ -10,3 +18,18 @@ class StatusSerializer(serializers.ModelSerializer):
             'content',
             'image'
         ]
+
+    # def validate_<field_name>(self, value):
+    # def validate_content(self, value):
+    #     if len(value) > 10000:
+    #         raise serializers.ValidationError("This is way too long")
+    #     return value
+
+    def validate(self, data):
+        content = data.get('content', None)
+        if content == '':
+            content = None
+        image = data.get('image', None)
+        if content is None and image is None:
+            raise serializers.ValidationError('Content or image is required.')
+
